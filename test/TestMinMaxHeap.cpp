21bf1ac8a05a2e4d65ba5da6ac7412a1ec23b17a
Zdravko Hvarlingov <zdravkohv@gmail.com>
2019-06-22 11:31:20 +0300

    Double equality comparison changed

--- test/TestMinMaxHeap.cpp
+++ test/TestMinMaxHeap.cpp
@@ -1,5 +1,12 @@
 #include "catch.hpp"
 #include "MinMaxHeap.hpp"
+#include <cmath>
+#include <limits>
+
+bool AreEqual(double first, double second)
+{
+	return std::fabs(first - second) < std::numeric_limits<double>::epsilon();
+}
 
 TEST_CASE("MinHeap push is correct", "[Heap]")
 {
@@ -14,23 +21,23 @@ TEST_CASE("MinHeap push is correct", "[Heap]")
 	{
 		minHeap.Push(10);
 		REQUIRE(minHeap.GetSize() == 1);
-		REQUIRE(minHeap.GetTop() == 10);
+		REQUIRE(AreEqual(minHeap.GetTop(), 10));
 
 		minHeap.Push(-10);
-		REQUIRE(minHeap.GetTop() == -10);
+		REQUIRE(AreEqual(minHeap.GetTop(), -10));
 
 		minHeap.Push(-3);
-		REQUIRE(minHeap.GetTop() == -10);
+		REQUIRE(AreEqual(minHeap.GetTop(), -10));
 
 		minHeap.Push(-11);
-		REQUIRE(minHeap.GetTop() == -11);
+		REQUIRE(AreEqual(minHeap.GetTop(), -11));
 	}
 	SECTION("Pushing equal numbers")
 	{
 		for (int i = 0; i < 100; i++)
 		{
 			minHeap.Push(10);
-			REQUIRE(minHeap.GetTop() == 10);
+			REQUIRE(AreEqual(minHeap.GetTop(), 10));
 		}
 	}
 	SECTION("Pushing in ascending order")
@@ -38,7 +45,7 @@ TEST_CASE("MinHeap push is correct", "[Heap]")
 		for (int i = 0; i < 100; ++i)
 		{
 			minHeap.Push(i + 1);
-			REQUIRE(minHeap.GetTop() == 1);
+			REQUIRE(AreEqual(minHeap.GetTop(), 1));
 		}
 	}
 	SECTION("Pushing in descending order")
@@ -46,7 +53,7 @@ TEST_CASE("MinHeap push is correct", "[Heap]")
 		for (int i = 99; i >= 0; --i)
 		{
 			minHeap.Push(i + 1);
-			REQUIRE(minHeap.GetTop() == i + 1);
+			REQUIRE(AreEqual(minHeap.GetTop(), i + 1));
 		}
 	}
 }
@@ -64,23 +71,23 @@ TEST_CASE("MaxHeap push is correct", "[Heap]")
 	{
 		maxHeap.Push(10);
 		REQUIRE(maxHeap.GetSize() == 1);
-		REQUIRE(maxHeap.GetTop() == 10);
+		REQUIRE(AreEqual(maxHeap.GetTop(), 10));
 
 		maxHeap.Push(-10);
-		REQUIRE(maxHeap.GetTop() == 10);
+		REQUIRE(AreEqual(maxHeap.GetTop(), 10));
 
 		maxHeap.Push(-3);
-		REQUIRE(maxHeap.GetTop() == 10);
+		REQUIRE(AreEqual(maxHeap.GetTop(), 10));
 
 		maxHeap.Push(11);
-		REQUIRE(maxHeap.GetTop() == 11);
+		REQUIRE(AreEqual(maxHeap.GetTop(), 11));
 	}
 	SECTION("Pushing equal numbers")
 	{
 		for (int i = 0; i < 100; i++)
 		{
 			maxHeap.Push(10);
-			REQUIRE(maxHeap.GetTop() == 10);
+			REQUIRE(AreEqual(maxHeap.GetTop(), 10));
 		}
 	}
 	SECTION("Pushing in ascending order")
@@ -88,7 +95,7 @@ TEST_CASE("MaxHeap push is correct", "[Heap]")
 		for (int i = 0; i < 100; ++i)
 		{
 			maxHeap.Push(i + 1);
-			REQUIRE(maxHeap.GetTop() == i + 1);
+			REQUIRE(AreEqual(maxHeap.GetTop(), i + 1));
 		}
 	}
 	SECTION("Pushing in descending order")
@@ -96,7 +103,7 @@ TEST_CASE("MaxHeap push is correct", "[Heap]")
 		for (int i = 99; i >= 0; --i)
 		{
 			maxHeap.Push(i + 1);
-			REQUIRE(maxHeap.GetTop() == 100);
+			REQUIRE(AreEqual(maxHeap.GetTop(), 100));
 		}
 	}
 }
@@ -109,7 +116,7 @@ TEST_CASE("MinHeap push and pop are correct", "[Heap]")
 	SECTION("Push and pop different numbers")
 	{
 		minHeap.Push(10);
-		REQUIRE(minHeap.GetTop() == 10);
+		REQUIRE(AreEqual(minHeap.GetTop(), 10));
 
 		minHeap.Pop();
 		REQUIRE(minHeap.GetSize() == 0);
@@ -119,11 +126,11 @@ TEST_CASE("MinHeap push and pop are correct", "[Heap]")
 			minHeap.Push(i + 1);
 		}
 		minHeap.Pop();
-		REQUIRE(minHeap.GetTop() == 2);
+		REQUIRE(AreEqual(minHeap.GetTop(), 2));
 
 		minHeap.Pop();
 		minHeap.Push(-1);
-		REQUIRE(minHeap.GetTop() == -1);
+		REQUIRE(AreEqual(minHeap.GetTop(), -1));
 	}
 	SECTION("Pushing equal numbers")
 	{
@@ -134,7 +141,7 @@ TEST_CASE("MinHeap push and pop are correct", "[Heap]")
 		for (int i = 0; i < 99; i++)
 		{
 			minHeap.Pop();
-			REQUIRE(minHeap.GetTop() == 10);
+			REQUIRE(AreEqual(minHeap.GetTop(), 10));
 		}
 	}
 }
@@ -147,7 +154,7 @@ TEST_CASE("MaxHeap push and pop are correct", "[Heap]")
 	SECTION("Push and pop different numbers")
 	{
 		maxHeap.Push(10);
-		REQUIRE(maxHeap.GetTop() == 10);
+		REQUIRE(AreEqual(maxHeap.GetTop(), 10));
 
 		maxHeap.Pop();
 		REQUIRE(maxHeap.GetSize() == 0);
@@ -157,11 +164,11 @@ TEST_CASE("MaxHeap push and pop are correct", "[Heap]")
 			maxHeap.Push(i + 1);
 		}
 		maxHeap.Pop();
-		REQUIRE(maxHeap.GetTop() == 9);
+		REQUIRE(AreEqual(maxHeap.GetTop(), 9));
 
 		maxHeap.Pop();
 		maxHeap.Push(-1);
-		REQUIRE(maxHeap.GetTop() == 8);
+		REQUIRE(AreEqual(maxHeap.GetTop(), 8));
 	}
 	SECTION("Pushing equal numbers")
 	{
@@ -172,7 +179,7 @@ TEST_CASE("MaxHeap push and pop are correct", "[Heap]")
 		for (int i = 0; i < 99; i++)
 		{
 			maxHeap.Pop();
-			REQUIRE(maxHeap.GetTop() == 10);
+			REQUIRE(AreEqual(maxHeap.GetTop(), 10));
 		}
 	}
 }
@@ -184,13 +191,13 @@ TEST_CASE("A lot of negative numbers", "[Heap]")
 	{
 		maxHeap.Push(-(i + 1));
 	}
-	REQUIRE(maxHeap.GetTop() == -1);
+	REQUIRE(AreEqual(maxHeap.GetTop(), -1));
 
 	for (int i = 0; i < 999999; i++)
 	{
 		maxHeap.Pop();
 	}
-	REQUIRE(maxHeap.GetTop() == -1000000);
+	REQUIRE(AreEqual(maxHeap.GetTop(), -1000000));
 
 	maxHeap.Pop();
 	REQUIRE(maxHeap.GetSize() == 0);
